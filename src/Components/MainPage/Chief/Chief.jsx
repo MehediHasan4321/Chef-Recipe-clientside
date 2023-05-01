33897d419662ea28b26983774c4964cd4b836806
MehediHasan4321 <126mehedi@gmail.com>
2023-05-02 00:41:52 +0600

    fetch data from local server

--- src/Components/MainPage/Chief/Chief.jsx
+++ src/Components/MainPage/Chief/Chief.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Chief = () => {
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/')
+        .then(res=>res.json())
+        .then(data=>setData(data))
+    },[])
+    
     return (
         <div className='container mx-auto'>
-            <h1>This is Chief Section</h1>
+            <h1 className='text-4xl font-semibold text-center my-12'>Our World famus Chefs</h1>
+            {
+                data.map(chef=><img key={chef.id} className='w-48 flex gap-5' src={chef.chef_img} alt="" />)
+            }
         </div>
     );
 };
--- src/Components/MainPage/Main/Main.jsx
+++ src/Components/MainPage/Main/Main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Main.css'
+import Chief from '../Chief/Chief';
 const Main = () => {
     return (
-        <div className='customBanner w-full h-screen'>
-            <div className='container mx-auto'>
+        <>
+            <div className='customBanner w-full h-screen'>
+                <div className='container mx-auto'>
+
+                </div>
 
             </div>
-        </div>
+            <Chief />
+        </>
     );
 };
 
